af372a3ec5b34f5e996a67838ecf1143fb2a4938
markwininger <41705235+markwininger@users.noreply.github.com>
2023-12-08 15:51:23 -0800

    initial commit

---
+++ C++/Section_7/Arrays/main.cpp
@@ -0,0 +1,37 @@
+// What is an array?
+// A compound data type or data structure
+// a collection of elements
+// All elements are of the same type
+// Each element can be accessed directly
+
+// Characteristics
+// ==========
+// Fixed size
+// Elements are all the same type
+// stored contiguously in memory
+// individual elemtns can be accessed by their position or index
+// first element is at index 0
+// last element is at index size-1
+// no checking to see if you are out of bounds
+// always initialize arrays
+// very efficient
+// iteration (looping) is often used to process
+
+// Declaring
+// ===============
+// Element_Type array_name [constant number of elements];
+// int test_scores [5];
+
+// int high_score_per_level [10];
+
+// const int days_in_year {365};
+// double hi_temperatures [days_in_year];
+
+// Initialization
+// ======================
+// Element_Type array_name [number of elements] {init list};
+// int test_scores [5] {100,95,99,87,88};
+// int high_score_per_level [10] {3,5}; // init to 3,5 and remaining to 0
+// const double days_in_year {365};
+// double hi_temperatures [days_in_year] {0}; // init all to zero
+// int another_array [] {1,2,3,4,5}; // size automatically calculated
--- C++/Section_Challenges/Section_6/main.cpp
+++ C++/Section_Challenges/Section_6/main.cpp
@@ -1,6 +1,8 @@
 /*
     Frank's Carpet Cleaning Service
-    Charges $30 per room
+    Charges:
+     $25 per small room
+     $30 per small room
     Sales tax rate is 6%
     Estimates are valid for 30 days
 
@@ -36,21 +38,30 @@ int main()
 {
     cout << "Hello, welcome to Frank's Carpet Cleaning Service" << endl;
     cout << "\nHow many rooms would you like cleaned? ";
+    cout << "\nSmall rooms? ";
 
-    int number_of_rooms{0};
-    cin >> number_of_rooms;
+    int number_of_small_rooms{0};
+    cin >> number_of_small_rooms;
 
-    const double price_per_room{30};
-    const double sales_tax{0.06};
+    cout << "\nLarge rooms? ";
+
+    int number_of_large_rooms{0};
+    cin >> number_of_large_rooms;
+
+    const double price_per_small_room{25};
+    const double price_per_large_room{35};
+    const double room_cost = (price_per_small_room * number_of_small_rooms) + (price_per_large_room * number_of_large_rooms);
+    const double sales_tax = room_cost * 0.06;
     const int estimate_expiry{30}; // days
-
     cout << "\nEstimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << number_of_rooms << endl;
-    cout << "Price per room: $" << price_per_room << endl;
-    cout << "Cost: $" << price_per_room * number_of_rooms << endl;
-    cout << "Tax: $" << price_per_room * number_of_rooms * sales_tax << endl;
+    cout << "Number of small rooms: " << number_of_small_rooms << endl;
+    cout << "Number of large rooms: " << number_of_large_rooms << endl;
+    cout << "Price per small room: $" << price_per_small_room << endl;
+    cout << "Price per large room: $" << price_per_large_room << endl;
+    cout << "Cost: $" << room_cost << endl;
+    cout << "Tax: $" << sales_tax << endl;
     cout << "=======================================" << endl;
-    cout << "Total estimate: $" << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * sales_tax) << endl;
+    cout << "Total estimate: $" << room_cost + sales_tax << endl;
     cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 
     cout << endl;
